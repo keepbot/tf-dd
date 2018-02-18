42521c1624ca00e5b78b1546c29c61d278c47e66
Dmitriy Ivanov <divanov@aligntech.com>
2018-02-18 22:23:47 +0300

    add(provider): add aws provider congfig\

--- production/config.tf
+++ production/config.tf
@@ -1,4 +1,6 @@
 variable "region"                 { }
+variable "aws_access_key"         { }
+variable "aws_secret_key"         { }
 
 terraform {
   backend "s3" {
@@ -6,4 +8,11 @@ terraform {
     key    = "production.tfstate"
     region = "eu-central-1"
   }
+}
+
+provider "aws" {
+  access_key  = "${var.aws_access_key}"
+  secret_key  = "${var.aws_secret_key}"
+  region      = "${var.region}"
+  max_retries = "11"
 }
